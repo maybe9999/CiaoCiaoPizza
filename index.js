f2e5952c14ec506bd223ecd8c737bc03750e1701
maybe9999 <joel.dasilva2015@outlook.com>
2024-06-24 21:35:41 -0300

    :)

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 const express = require('express');  // Importamos el modulo express
-const path = require('path');
-const morgan = require('morgan');
+const path = require('path'); //
+const morgan = require('morgan'); //Info por consola sobre las peticiones entrantes
+const muestraSeccion = require('./routes/redirect'); //Contiene ubicación de los index y devuelve archivos
+
 const app = express();
 
 
-//configuración
-app.set("port", 5001)
-app.use(morgan('dev'));
-const muestraSeccion = require('./routes/redirect');
+//Configuración y Middleware
+app.set("port", 5001)//Se setae o guarda en una "variable", denominada por conveniencia port, el puerto...
 
+//Este modulo con el valor "dev" se encarga de mostrar por consola: Petición(Get, post, etc), ruta solicitada (/nosotros), Codigo de respuesta(200, 404, etc), y tiempo que tomo la peticion...
+app.use(morgan('dev')); 
 
-//Middleware
-app.use(express.json());
+app.use(express.json()); // No puedo definir bien el uso. pero permite manejar json creo.
+
 
 //Routes
 app.get('/', muestraSeccion);
@@ -20,11 +22,13 @@ app.get('/productos', muestraSeccion);
 app.get('/nuestraCarta',  muestraSeccion);
 app.get('/contacto', muestraSeccion);
 app.get('/login', muestraSeccion);
-app.get('/dashboard', muestraSeccion); //Esta ruta no se si debería manejarse desde aca, creo que no
+app.get('/dashboard', muestraSeccion); //Esta ruta no se si debería manejarse desde aca, creo que no!!
 
 //Si ninguna de las rutas coincide usara esta carpeta y por defecto devolverá el archivo index que encuentre
 app.use(express.static(path.join(__dirname, 'public'))); //Configuramos para servir archivos estáticos desde esta carpeta...
 
-app.listen(app.get("port") , () => {
+
+//Se le indica mediante .listen() que use el puerto X para escuchar o recibir peticiones, get post etc...
+app.listen(app.get("port") , () => { 
     console.log(`Servidor ejecutándose en el puerto ${app.get("port")}`)
 });
--- routes/redirect.js
+++ routes/redirect.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const crud = require("../user_controller/Crud"); //Para hacer consultas a la bd
+
 
 // Resuelve la ruta absoluta hacia los archivos index de las diferentes secciones
 const routesPublic = {
@@ -11,15 +13,26 @@ const routesPublic = {
     dashboard : path.resolve(__dirname, '../public/dashboard/index.html')
 };
 
+
+//Devuelve el archivo index.html de la sección correspondiente en base a la solicitud.
 function mostrarSeccion(req, res) {
     //Obtiene mediante "req.path" el nombre del path o sección solicitada en la consulta(nosotros, productos, etc.), 
     //mediante .replace() se remplazan todos los / de la string usando una "expresión regular" como primer argumento y en el segundo definimos que debe estar vacio "".
     let endPointActual = req.path.replace(/(\/)/gm,""); 
 
+
     console.log(`1 impresión en ${endPointActual}`);
     
+
+    //Lógica de si no estas logeado como admin te mandamos al login.
+    if (endPointActual == "dashboard"){ 
+        //El que se de maña que lo que haga :) !!!!!!!
+        crud(req, res)
+    }
+
     // Envía el archivo index.html como respuesta
     res.sendFile(routesPublic[endPointActual]);
+
 }
 
 module.exports = mostrarSeccion;
