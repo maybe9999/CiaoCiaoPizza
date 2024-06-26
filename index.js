7932299fbbb1a7f5b26ebbd8d1c75fd7847ef879
maybe9999 <joel.dasilva2015@outlook.com>
2024-06-26 20:08:17 -0300

    Merge de ramas patch 2  patch 3

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');  // Importamos el modulo express
 const path = require('path'); //
 const morgan = require('morgan'); //Info por consola sobre las peticiones entrantes
+
+//--- creado para obtener el acceso mediante el login
+const session = require('./data_base/db'); // Importamos la conexión a la base de datos
+//---
+
 const muestraSeccion = require('./routes/redirect'); //Contiene ubicación de los index y devuelve archivos
 
 const app = express();
@@ -12,7 +17,12 @@ app.set("port", 5001)//Se setae o guarda en una "variable", denominada por conve
 //Este modulo con el valor "dev" se encarga de mostrar por consola: Petición(Get, post, etc), ruta solicitada (/nosotros), Codigo de respuesta(200, 404, etc), y tiempo que tomo la peticion...
 app.use(morgan('dev')); 
 
-app.use(express.json()); // No puedo definir bien el uso. pero permite manejar json creo.
+app.use(express.json()); //
+
+//----
+app.use(express.urlencoded({ extended: false })); // Middleware para manejar URL-encoded
+//----
+
 
 
 //Routes
@@ -21,20 +31,24 @@ app.get('/nosotros', muestraSeccion);
 app.get('/productos', muestraSeccion);
 app.get('/nuestraCarta',  muestraSeccion);
 app.get('/contacto', muestraSeccion);
-app.get('/login', muestraSeccion);
-app.get('/dashboard', muestraSeccion); //Esta ruta no se si debería manejarse desde aca, creo que no!!
 
 
 // Ruta para manejar el login
 app.post('/login', (req, res) => {
     const { username, password } = req.body;
-    if (username === 'admin' && password === 'admin') {
-        // Si las credenciales son válidas, redirigir al dashboard
-        res.redirect('/dashboard');
-    } else {
-        // Si las credenciales son inválidas, redirigir al login
-        res.redirect('/login');
-    }
+    const query = 'SELECT * FROM Usuario WHERE username = ? AND password = ?';
+    session.query(query, [username, password], (err, results) => {
+        if (err) {
+            console.error('Error ejecutando la consulta:', err);
+            res.status(500).json({ success: false, message: 'Error en el servidor' });
+            return;
+        }
+        if (results.length > 0) {
+            res.json({ success: true });
+        } else {
+            res.json({ success: false, message: 'Credenciales incorrectas' });
+        }
+    });
 });
 
 
@@ -46,5 +60,3 @@ app.use(express.static(path.join(__dirname, 'public'))); //Configuramos para ser
 app.listen(app.get("port") , () => { 
     console.log(`Servidor ejecutándose en el puerto ${app.get("port")}`)
 });
-
-
