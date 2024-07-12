57565d084a3f2e355120381f08a05e437380ac9d
Matias Da Silva <joel.dasilva2015@outlook.com>
2024-07-12 00:45:04 -0300

    .

--- data_base/db.js
+++ data_base/db.js
@@ -14,8 +14,8 @@ function handleDisconnect() {
     //se llama al método connect y mediante una función anónima verificamos si la conexión se realizo correctamente
     connection.connect((err) => {
         if (err) {
-            console.log("ERROR!!!, No se pudo conectar a la Base de Datos.\n Error:", err);
-            setTimeout(handleDisconnect, 2000);
+            console.log("ERROR!!!, No se pudo conectar a la Base de Datos.\n Error:", err,"--------/nCodigo de error:", err.code,"------/n");
+            setTimeout(handleDisconnect(), 2000);
         }else{
             console.log("Conectado con éxito a la base de datos");
         }
